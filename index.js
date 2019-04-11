6f1a890f32372a6d2fb1ad6c4f663bf5e9d045b4
Andrew <andrew@merison.io>
2019-04-11 12:17:17 -0700

    Made changes to shut Bryce up

--- index.js
+++ index.js
@@ -1,137 +1,119 @@
 /**
- * Google Ads:
- *  - Unique String for URL filtering:  'googleads.g.doubleclick.net'
- *  - Conversion ID Expression:         parsedUrl.pathname.split('/')[3];
- * Google Analytics:
- *  - Unique String for URL filtering:  'google-analytics.com'
- *  - UA Number Expression:             parsedUrl.query.tid;
- *  - HitType Expression:               parsedUrl.query.t;
- * Facebook Pixel:
- *  - Unique String for URL filtering:  'facebook.com/tr/'
- *  - Pixel ID Expression:              parsedUrl.query.id;
- *  - Event Type Expression:            parsedUrl.query.ev;
- * Bing Ads:
- *  - Unique String for URL filtering:  'bat.bing.com/action'
- *  - UET ID Expression:                parsedUrl.query.ti;
- *  - UET HitType Expression:           parsedUrl.query.evt;
- */
+* This tool is meant to serve as a simple proof-of-concept and
+* has not been built for expansion/actual real-world use.
+*
+* 
+* Google Ads:
+*  - Unique String for URL filtering:  'googleads.g.doubleclick.net'
+*  - Conversion ID Expression:         parsedUrl.pathname.split('/')[3];
+* Google Analytics:
+*  - Unique String for URL filtering:  'google-analytics.com'
+*  - UA Number Expression:             parsedUrl.query.tid;
+*  - HitType Expression:               parsedUrl.query.t;
+* Facebook Pixel:
+*  - Unique String for URL filtering:  'facebook.com/tr/'
+*  - Pixel ID Expression:              parsedUrl.query.id;
+*  - Event Type Expression:            parsedUrl.query.ev;
+* Bing Ads:
+*  - Unique String for URL filtering:  'bat.bing.com/action'
+*  - UET ID Expression:                parsedUrl.query.ti;
+*  - UET HitType Expression:           parsedUrl.query.evt;
+*/
 
-// Dependencies
 const puppeteer = require('puppeteer');
 const URL = require('url');
 
-// Constants for construction of each report
 const GOOGLEANALYTICS = 'Google Analytics';
 const FACEBOOK = 'Facebook Ads';
 const BING = 'Bing Ads' 
 const GOOGLEADS = 'Google Ads';
 
-// Sites to check
 const sites = [
-  'https://www.logicalposition.com/',
-  'https://www.millerplastics.com/',
-  'https://www.hannaandersson.com/baby-girl/56548-V98.html?cgid=baby-girl&dwvar_56548-V98_color=V98'];
+	'https://www.logicalposition.com/',
+	'https://www.millerplastics.com/',
+	'https://www.hannaandersson.com/baby-girl/56548-V98.html?cgid=baby-girl&dwvar_56548-V98_color=V98'
+];
 
+const checkPage = async site => {
+	console.log('Started testing for ', site);
+	const trackingInformation = {
+		'Google Analytics': {},
+		'Facebook Ads': {},
+		'Bing Ads': {},
+		'Google Ads': []
+	};
+	
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	
+	page.on('request', request => {
+		const url = request._url;
+		
+		if (url.indexOf('google-analytics.com') > -1) {
+			const parsedUrl = URL.parse(url, true);
+			
+			if (typeof parsedUrl.query.tid != 'undefined' && typeof parsedUrl.query.t != 'undefined') {
+				console.log(`${site} - GOOGLE ANALYTICS: ${parsedUrl.query.tid} - ${parsedUrl.query.t.toUpperCase()}`);
+				storeData(GOOGLEANALYTICS, { id: parsedUrl.query.tid, hitType: parsedUrl.query.t });
+			}
+		} else if (url.indexOf('facebook.com/tr/') > -1) {
+			const parsedUrl = URL.parse(url, true);
+			
+			if (typeof parsedUrl.query.id != 'undefined' && typeof parsedUrl.query.id != 'undefined') {
+				console.log(`${site} - FACEBOOK PIXEL: ${parsedUrl.query.id} - ${parsedUrl.query.ev.toUpperCase()}`);
+				storeData(FACEBOOK, { id: parsedUrl.query.id, hitType: parsedUrl.query.ev });
+			}
+		} else if (url.indexOf('googleads.g.doubleclick.net') > -1) {
+			const parsedUrl = URL.parse(url, true);
 
-/**
- * Main logic for the tool. Opens a headless browser for each site passed to it,
- * listens to all network requests, and filters for any matching the 4 services
- * the tool currently cares about.
- * 
- * If any requests match it pulls necessary data from them, then builds the JSON
- * object used for the final report.
- * 
- */
-const checkPage = async (site) => {
-  console.log('Started testing for ', site);
-  let trackingInformation = {
-    'Google Analytics': {},
-    'Facebook Ads': {},
-    'Bing Ads': {},
-    'Google Ads': []
-  };
-
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  page.on('request', (request) => {
-    // Storing for ease of use
-    let url = request._url;
-
-    // These if statements check the request URL against known URLs for the 4 services we care about right now.
-    if (url.indexOf('google-analytics.com') > -1) {
-      let parsedUrl = URL.parse(url, true);
-
-      // Check if the URL contained query string values for the tracking ID and hit type
-      if (typeof parsedUrl.query.tid != 'undefined' && typeof parsedUrl.query.t != 'undefined') {
-        console.log(`${site} - GOOGLE ANALYTICS: ${parsedUrl.query.tid} - ${parsedUrl.query.t.toUpperCase()}`);
-        storeData(GOOGLEANALYTICS, { id: parsedUrl.query.tid, hitType: parsedUrl.query.t });
-      }
-    } else if (url.indexOf('facebook.com/tr/') > -1) {
-      let parsedUrl = URL.parse(url, true);
-
-      // Check if the URL contained query string values for the pixel ID and hit type
-      if (typeof parsedUrl.query.id != 'undefined' && typeof parsedUrl.query.id != 'undefined') {
-        console.log(`${site} - FACEBOOK PIXEL: ${parsedUrl.query.id} - ${parsedUrl.query.ev.toUpperCase()}`);
-        storeData(FACEBOOK, { id: parsedUrl.query.id, hitType: parsedUrl.query.ev });
-      }
-    } else if (url.indexOf('googleads.g.doubleclick.net') > -1) {
-      let parsedUrl = URL.parse(url, true);
-      let conversionId = parsedUrl.pathname.split('/')[3];
-
-      // Check if the URL contained query string values for the conversion ID
-      console.log(`${site} - GOOGLE ADS REMARKETING: ${conversionId}`);
-      storeData(GOOGLEADS, { id: conversionId });
-    } else if (url.indexOf('bat.bing.com/action') > -1) {
-      let parsedUrl = URL.parse(url, true);
-
-      // Check if the URL contained query string values for the tracking ID and hit type
-      if (typeof parsedUrl.query.ti != 'undefined' && typeof parsedUrl.query.evt != 'undefined') {
-        console.log(`${site} - BING UET: ${parsedUrl.query.ti} - ${parsedUrl.query.evt}`);
-        storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt });
-      }
-    }
-  });
-
-  // Sends the headless browser to the URL passed into the function
-  await page.goto(site);
-  await browser.close();
-
-  // Stores the newly built report object in the full report
-  reportJson[site] = trackingInformation;
-
-  // Function that builds the report objects for us
-  function storeData(platform, dataObj) {
-    let { id, hitType } = dataObj;
-    let info = trackingInformation[platform];
-    if (typeof id != 'undefined' && typeof hitType != 'undefined') {
-      info[id] = (typeof info[id] != 'undefined')
-                                          ? info[id]
-                                          : {};
-      info[id][hitType] = (typeof info[id][hitType] != 'undefined')
-                                                  ? info[id][hitType] + 1
-                                                  : 1; 
-    } else if (typeof id != 'undefined') {
-      if (!info.includes(id)) {
-        info.push(id);
-      }
-    }
-  }
+			if (parsedUrl.pathname.split('/').length > 4 && parsedUrl.pathname.split('/')[3].length === 9) {
+				const conversionId = parsedUrl.pathname.split('/')[3];
+			
+				console.log(`${site} - GOOGLE ADS REMARKETING: ${conversionId}`);
+				storeData(GOOGLEADS, { id: conversionId });
+			}
+		} else if (url.indexOf('bat.bing.com/action') > -1) {
+			const parsedUrl = URL.parse(url, true);
+			
+			if (typeof parsedUrl.query.ti != 'undefined' && typeof parsedUrl.query.evt != 'undefined') {
+				console.log(`${site} - BING UET: ${parsedUrl.query.ti} - ${parsedUrl.query.evt}`);
+				storeData(BING, { id: parsedUrl.query.ti, hitType: parsedUrl.query.evt });
+			}
+		}
+	});
+	
+	await page.goto(site);
+	await browser.close();
+	
+	reportJson[site] = trackingInformation;
+	
+	function storeData(platform, dataObj) {
+		let { id, hitType } = dataObj;
+		let info = trackingInformation[platform];
+		if (typeof id != 'undefined' && typeof hitType != 'undefined') {
+			info[id] = (typeof info[id] != 'undefined')
+				? info[id]
+				: {};
+			info[id][hitType] = (typeof info[id][hitType] != 'undefined')
+				? info[id][hitType] + 1
+				: 1; 
+		} else if (typeof id != 'undefined') {
+			if (!info.includes(id)) {
+				info.push(id);
+			}
+		}
+	}
 };
 
-// Initializing the report object and an array to keep track of our promises
 let reportJson = {};
 let promiseArr = [];
 
-// Loop through every site, create an entry in the main report Object,
-// then run the site through the main function and store the resulting promise in an array
-sites.forEach((site) => {
-    reportJson[site] = {};
-    promiseArr.push(checkPage(site));
+sites.forEach(site => {
+	reportJson[site] = {};
+	promiseArr.push(checkPage(site));
 });
 
-// Once every promise in the array has been resolved, print the full report Object to the console.
 Promise.all(promiseArr).then(() => {
-  console.log('All done! Results:');
-  console.dir(reportJson, { depth: null });
+	console.log('All done! Results:');
+	console.dir(reportJson, { depth: null });
 });
